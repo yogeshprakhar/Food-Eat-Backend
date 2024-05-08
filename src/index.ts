050b166a019eee943e8ae66fbfda99c250e60e2d
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-08 16:07:35 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute"
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database"))
+  .catch((e)=> console.log("Error occured",e))
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/my/user",myUserRoute);
+
+app.listen(7000, () => {
+  console.log("server started at localhost 7000");
+});
